48777108d559638689ae52b6cac55c405d3a1c68
phkhaikk <phkhaikk@gmail.com>
2024-08-15 17:03:07 +0700

    comit from master

--- app/models/db_product_category.py
+++ app/models/db_product_category.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from app.models.base import BaseModel
 
 class DbProductCategory(BaseModel):
